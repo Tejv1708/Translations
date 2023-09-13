960cf1970c8be65192322ef3c48481ded6867d75
Tejv1708 <narukatejveer@gmail.com>
2023-09-13 11:04:34 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contactus from "./pages/contact";
+import Aboutus from "./pages/about";
+import Home from "./pages/home";
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/contactus" element={<Contactus />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Menu from "../component/menu";
+import Languageoption from "../component/language-dropdown";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+const Home = () => {
+  const { t } = useTranslation();
+  const handleClick = (e) => {
+    i18next.changeLanguage(e.target.value);
+  };
+  return (
+    <div className=" bg-slate-500">
+      <Menu />
+      <Languageoption onChange={(e) => handleClick(e)} />
+      <h1>
+        {t("welcome")} {t("home")}
+      </h1>
+    </div>
+  );
+};
+export default Home;
